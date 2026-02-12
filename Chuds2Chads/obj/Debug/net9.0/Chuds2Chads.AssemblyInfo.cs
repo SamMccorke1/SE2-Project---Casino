5e76f468d5c4b22dec2aff91f2387a04fccbcd13
llitz02 <113472551+llitz02@users.noreply.github.com>
2026-02-12 11:23:42 -0500

    ?

--- Chuds2Chads/obj/Debug/net9.0/Chuds2Chads.AssemblyInfo.cs
+++ Chuds2Chads/obj/Debug/net9.0/Chuds2Chads.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chuds2Chads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2b389f9278ae6607e6a6f21edd6161ffea28b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87af28d6c1584e5a7f0fc6386aa8c996cd2be701")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chuds2Chads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chuds2Chads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
